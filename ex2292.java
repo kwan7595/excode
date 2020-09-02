9d78838a6ef01e4b8f533eeaafb9bb1a4909d256
KwanHee <kwan7595@naver.com>
2020-09-02 14:37:12 +0000

    ex2292 src--java 4th edit

--- ex2292.java
+++ ex2292.java
@@ -5,7 +5,7 @@ public class ex2292{
 		int n=s.nextInt();
 		int t=0;
 		for(t=1;;t++){
-			if((n-1)/3<(t*(t-1))||(n-1)/3==(t*(t-1))){
+			if((n-1)<=3*t*(t-1)){
 				System.out.printf("%d",t);
 				break;
 			}
