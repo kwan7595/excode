f324f6ddb3c964d57f1a64b42146b359e507956f
KwanHee <kwan7595@naver.com>
2020-09-05 14:00:44 +0000

    ex10951 1st edit

--- ex10951.java
+++ ex10951.java
@@ -3,7 +3,7 @@ public class ex10951{
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
 		int a=0,b=0;
-		char temp;
+		String temp;
 		for(;;){
 			a=s.nextInt();
 			temp=s.next();
