d3092c915354403aa5c9e9d45582c1dc84eaae11
KwanHee <kwan7595@naver.com>
2020-09-05 14:38:53 +0000

    ex10818 1st edit

--- ex10818.java
+++ ex10818.java
@@ -4,17 +4,12 @@ public class ex10818{
 		Scanner s = new Scanner(System.in);
 		int num=s.nextInt();
 		int no[] = new int[num];
-		int temp=0,max=0,min=0,i=0;
-		for(i=0;i<num;i++) no[i]=s.nextInt();
+		int max=0,min=1000000,i=0;
 		for(i=0;i<num;i++){
-			if(no[i]>=temp) temp=no[i];
+			no[i]=s.nextInt();
+			if(no[i]>=max) max=no[i];
+			if(no[i]<=min) min=no[i];
 		}
-		max=temp;
-		temp=no[0];
-		for(i=0;i<num;i++){
-			if(no[i]<=temp) temp=no[i];
-		}
-		min=temp;
 		System.out.printf("%d %d",min,max);
 	}
 }
