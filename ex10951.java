0dc21edadb3a6bc40b822737a69eb5a572b4820f
KwanHee <kwan7595@naver.com>
2020-09-05 13:59:59 +0000

    ex10951 java src

---
+++ ex10951.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ex10951{
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		int a=0,b=0;
+		char temp;
+		for(;;){
+			a=s.nextInt();
+			temp=s.next();
+			b=s.nextInt();
+			temp=s.next();
+			System.out.printf("%d\n",a+b);
+			if(s.next()=="\n") break;
+		}
+	}
+}
