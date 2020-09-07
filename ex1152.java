f25d100fe9a1a3ef65beb5799b27a7077bee3ffe
KwanHee <kwan7595@naver.com>
2020-09-07 13:23:12 +0000

    ex1152 1st edit

--- ex1152.java
+++ ex1152.java
@@ -7,8 +7,14 @@ public class ex1152{
 		strData = s.nextLine();
 		StringTokenizer st= new StringTokenizer(strData," ");
 		int count=0;
-		while(st.hasMoreTokens()) count++;
+		while(st.hasMoreTokens()){
+			st.nextToken();//using nextToken() method to read next Token.
+			count++;//counting the number of token in strData
+		}
 		System.out.printf("%d",count);
 	}
 }
-		
+/* using Tokenizer methods
+StringTokenizer(a,b) --> a:string data, b:delimiter(can be more then 1 delimiter)
+hasMoreTokens()--> returns true if there's a token to be returned, else returns false
+*/
