04088cc6484c3a38b32bd83087cd1ad9bd476edf
KwanHee <kwan7595@naver.com>
2020-09-18 14:28:46 +0000

    ex2798 java src

---
+++ ex2798.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ex2798{
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		int n=0,m=0,i=0,j=0,k=0,temp=0,sum=0;
+		n=s.nextInt();
+		m=s.nextInt();
+		int card[] = new int[n];
+		for(i=0;i<n;i++) card[i]=s.nextInt();
+		for(i=0;i<n;i++){
+			for(j=1;j<n-i;j++){
+				for(k=1;k<n-i-j;k++){
+					temp=card[i]+card[i+j]+card[i+j+k];
+					if(sum<=temp&&temp<=m) sum=temp;
+				}
+			}
+		}
+		System.out.printf("%d",sum);
+	}
+}
