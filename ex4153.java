a1f59962331a70f98a3f420beb5434bc9af3c4dc
KwanHee <kwan7595@naver.com>
2020-12-15 14:30:56 +0000

    ex4153 breaking algorithm editted--using counter

--- ex4153.java
+++ ex4153.java
@@ -5,10 +5,14 @@ public class ex4153{
 		Scanner s = new Scanner(System.in);
 		int a[] = new int[3];
 		int b[] = {0,0,0};
+		int counter=0;
 		for(;;){
-			for(int i=0;i<3;i++) a[i]=s.nextInt();
+			for(int i=0;i<3;i++){
+				a[i]=s.nextInt();
+				if(a[i]==0) counter++;
+			}
+			if(counter==3) break;
 			Arrays.sort(a);
-			if(a==b) break;
 			if(a[2]*a[2]==a[1]*a[1]+a[0]*a[0]) System.out.print("right\n");
 			else System.out.print("wrong\n");
 		}
